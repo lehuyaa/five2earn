42dd00521b04555c3f9fdb10ec233d8e49086b5d
datpt.haui <datpt.haui>
2024-03-14 20:46:18 +0700

    config api game 1

---
+++ src/api/modules/api-app/api_game1.js
@@ -0,0 +1,92 @@
+import request from '../../request';
+
+//1
+export const checkFirstJoinAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//2
+export const checkStatusMatchPendingAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//3
+export const updateMatchStatusToCloseAPI = async (params) => {
+    return await request.put(`api/odt-on-outs`, params);
+};
+
+//4
+export const checkExistMatchProgressAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//5 same 15 same 17
+export const updateMatchStatusToLoseAPI = async (params) => {
+    return await request.put(`api/odt-on-outs`, params);
+};
+
+//6
+export const checkCompleteMatchAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//7
+export const checkMatchPendingAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//8
+export const updateStatusToProgressAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//9
+export const updateMatchStatusToProgressAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//10
+export const updateStatusToPendingAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//11
+export const checkCanStartGameAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//12
+export const updateStatusCloseWhenTimeoutAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//13 same 16
+export const checkProgressionCompetitorAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//14
+export const updateStatusToLoseAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
+//15
+// export const updateMatchStatusToLoseAPI = async (params) => {
+//     return await request.get(`api/odt-on-outs`, { params });
+// };
+
+//16
+// export const checkExistMatchReady = async (params) => {
+//     return await request.get(`api/odt-on-outs`, { params });
+// };
+
+//17
+// export const checkExistMatchReady = async (params) => {
+//     return await request.get(`api/odt-on-outs`, { params });
+// };
+
+//18
+export const updateStatusToWinAPI = async (params) => {
+    return await request.get(`api/odt-on-outs`, { params });
+};
+
---
+++ src/api/request.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const TOKEN =
+  '5f16f99c48ad8384188157dc189e0a6abf31cf4aa6b574fea055e387223aacf33067ca9e05bd48e5f03aa56b2657585aa75ed6dabbcdb47d5e6ddccf9922b91848c94ad399421c2e6e2673f99b9eb480fb9b3215e6ff2a2b2bc101528cce0e92ebfea46ef2b3741f3510873601eaf51ea0b410cbfb8c7577ceb758ed74ece18f';
+const request = axios.create({
+  baseURL: 'http://192.168.1.35:1337/',
+  timeout: 8000,
+  headers: {Accept: '*/*'},
+});
+// for multiple requests
+
+request.interceptors.request.use(
+  async (config) => {
+    // Do something before API is sent
+    const token = TOKEN;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    // Do something with API error
+    return Promise.reject(error);
+  },
+);
+
+request.interceptors.response.use(
+  (response) => response.data,
+  async (error) => {
+    // Check network first
+    // status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    const {response} = error || {};
+    const {data} = response || {};
+    const {errorMessage, errorKey} = data || {};
+
+    error.message = errorMessage;
+    error.keyMessage = errorKey || '';
+    return Promise.reject(error.message);
+  },
+);
+
+export default request;
